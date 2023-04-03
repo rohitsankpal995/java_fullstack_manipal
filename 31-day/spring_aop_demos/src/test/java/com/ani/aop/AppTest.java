8528a4650767b194413f1fd79e90eadbbb57f63f
Rohit <rohitsankpal2000@gmail.com>
2023-04-03 16:12:21 +0530

    spring : aop

---
+++ 31-day/spring_aop_demos/src/test/java/com/ani/aop/AppTest.java
@@ -0,0 +1,16 @@
+package com.ani.aop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
+
