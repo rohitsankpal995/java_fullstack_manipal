ee974013047858177504b4bdf5273e21a0a599d8
Rohit <rohitsankpal2000@gmail.com>
2023-04-04 15:16:39 +0530

    spring : mvc

---
+++ 32-day/demo/src/main/java/com/rohit/web/controller/HiController.java
@@ -0,0 +1,15 @@
+package com.rohit.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/hi")
+@Controller // http://localhost:8080/hi
+public class HiController {
+
+    @GetMapping("/sayHi") // request with the HTTP GET method
+    public String hi() { // http://localhost:8080/hi/sayHi
+        return "hi";
+    }
+}
