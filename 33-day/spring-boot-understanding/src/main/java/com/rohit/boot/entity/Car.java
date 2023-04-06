c8b8f82f4b4d37eb3bed3e1806f748e9eb20aac9
Rohit <rohitsankpal2000@gmail.com>
2023-04-06 09:19:14 +0530

    add starter

---
+++ 33-day/spring-boot-understanding/src/main/java/com/rohit/boot/entity/Car.java
@@ -0,0 +1,14 @@
+package com.rohit.boot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class Car {
+    private Long id;
+    private String make;
+    private String model;
+}
---
+++ 33-day/spring-boot-understanding/src/main/java/com/rohit/boot/service/CarService.java
@@ -0,0 +1,33 @@
+package com.rohit.boot.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import com.rohit.boot.entity.Car;
+
+@PropertySource("myapp.yml")
+@PropertySource("other.properties")
+@Service
+public class CarService {
+
+    @Value("${testing.accounts.ws}")
+    private String website;
+
+    @Value("${testing.accounts.mail}")
+    private String mail;
+
+    @Value("${server.port}")
+    private Integer port;
+    public void createNewCar(Long id, String make, String model) {
+
+        Car car = new Car();
+        car.setId(id);
+        car.setMake(make);
+        car.setModel(model);
+        System.out.println("Car Company Website: " + website);
+        System.out.println("Car Company Email: " + mail);
+        System.out.println("Car Company Port: " + port);
+        System.out.println(car.toString());
+    }
+}
