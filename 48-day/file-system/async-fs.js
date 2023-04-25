13d22b171a5ed030ec41038d72b0efdd39538035
Rohit <rohitsankpal2000@gmail.com>
2023-04-25 15:07:22 +0530

    js-fundamentals

---
+++ 48-day/file-system/async-fs.js
@@ -0,0 +1,10 @@
+const fs = require('fs/promises')
+
+const content = 'Some Data '+ new Date()
+
+console.log('👉 Writing File')
+const prm = fs.writeFile('./async.txt', content)
+prm.then( () => console.log('✅ Data written successfully') )
+console.log('✅ Done write operation') 
+
+fs.readFile('./async.txt').then( dt => console.log(dt))
