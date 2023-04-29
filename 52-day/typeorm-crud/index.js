d2018051275b0583158564d36ca642aacb22e49c
Rohit <rohitsankpal2000@gmail.com>
2023-04-29 11:54:18 +0530

    typeorm:query builders

--- 52-day/typeorm-crud/index.js
+++ 52-day/typeorm-crud/index.js
@@ -5,7 +5,7 @@ const dataSource = new typeorm.DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "aniruddha",
+    password: "Rohit@2000",
     database: "nodejsdb",
     synchronize: true,
     entities: [ require("./Post"), require("./Category")],
@@ -16,13 +16,15 @@ dataSource.initialize().then(() => {
     console.log(`✅ Successfully Created`)
 
     var category1 = {
-        name: "TypeScript",
+        id: 1,
+        name: "Python",
     }
     var category2 = {
         name: "Programming",
     }
 
     const categoryRepository = dataSource.getRepository('Category')
+
     // categoryRepository.save(category1).then(saved => {
     //     console.log(`✅ Category Saved`)
     // }).catch(console.log)
@@ -30,8 +32,17 @@ dataSource.initialize().then(() => {
     //     console.log(`✅ Category Saved`)
     // }).catch(console.log)
 
-    categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
+    // categoryRepository.find().then(categories => console.log(categories)).catch(console.log)
 
+    // categoryRepository.delete(2).then(res => console.log(`✅ Deleted Successfully`)).catch(console.log)
+
+    categoryRepository.query('select * from categories')
+                        .then(ctgs => console.log(ctgs))
+                        .catch(console.log)
+
+    const categories = categoryRepository.createQueryBuilder('Category').where('Category.name like :nm', {nm: '%P%'})
+    categories.getMany().then(ctgs => console.log(ctgs))
+    
 }).catch(error => {
     console.log("❌ Error: ", error)
 })
